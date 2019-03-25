ef1aa1655c26af25798be24693c5c8df13632f1c
Daniel Gazcon <daniel.gazcon27@gmail.com>
2019-03-25 08:11:24 -0400

    alert session close

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HomeService } from '../../api/home.service'
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -12,7 +13,8 @@ export class HomePage implements OnInit {
 	public items: Array<any>
 	constructor(
 		private router: Router,
-		private homeService: HomeService
+		private homeService: HomeService,
+		private alertController: AlertController
 	) {}
 
 	ngOnInit(){
@@ -25,5 +27,29 @@ export class HomePage implements OnInit {
 			this.items = items.data
 			console.log(items)
 		})
+	}
+
+ 	async logout() {
+		const alert = await this.alertController.create({
+			header: 'Mr Help',
+			message: '¿Desea cerrar sesion?',
+				buttons: [{
+					text: 'Cancel',
+					role: 'cancel',
+					cssClass: 'secondary',
+						handler: (blah) => {
+						console.log('Confirm Cancel: blah');
+					}
+				},{
+					text: 'Okay',
+					handler: () => {
+						localStorage.clear();
+						this.router.navigate(['/login']);
+					}
+				}
+			]
+		});
+
+		await alert.present();
 	}
 }
